06c974f07824ed05a0ab506736c9ed721b87442b
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-26 18:16:22 -0800

    largest of four

---
+++ 236_06_largestOfFour.js
@@ -0,0 +1,18 @@
+function largestOfFour(arr) {
+  // You can do this!
+  var result = [];
+
+  for (var i = 0; i < arr.length; i ++) {
+
+    var greatest = 0;
+    for (var j = 0; j < arr[i].length; j ++){
+      if (arr[i][j] > greatest) {
+        greatest = arr[i][j];
+      }
+    }
+    result.push(greatest);
+  }
+  return result;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
