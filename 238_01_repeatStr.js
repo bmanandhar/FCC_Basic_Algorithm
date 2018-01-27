5e2a144069816dd36b48f93f534a29bae37ce820
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-26 19:13:28 -0800

    repeatStr()

--- 238_01_repeatStr.js
+++ 238_01_repeatStr.js
@@ -1,9 +1,24 @@
-
+/*
 function repeatStringNumTimes(str, num) {
   // repeat after me
   if (num > 0) {
     result = str.repeat(num);
 
+    return result;
+  }
+  return "";
+}
+
+repeatStringNumTimes("abc", 3);
+*/
+
+function repeatStringNumTimes(str, num) {
+  // repeat after me
+  var result = "";
+  if (num > 0) {
+    for (var i = 0; i < num; i ++) {
+        result += str;
+    }
     return result;
   }
   return "";
