0dcd2bfb9935cb199f4669f850771c4d0c554a67
Bijaya Manandhar <bijayamanandhar@Bijayas-MacBook-Pro.local>
2018-01-27 06:43:08 -0800

    slices the given aray into group of arrays

---
+++ 241_01_chunkArrayInGroups.js
@@ -0,0 +1,12 @@
+function chunkArrayInGroups(arr, size) {
+  // Break it up.
+  var result = [];
+  var i = 0;
+
+  while (i < arr.length) {
+    result.push(arr.slice(i, i+size));
+    i += size;
+  }
+  return result;
+}
+chunkArrayInGroups(["a", "b", "c", "d"], 2);
